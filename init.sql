028f88bdc0c1f0e7d9a02ae877f8dde710187eb1
mniudanri <37972031+mniudanri@users.noreply.github.com>
2025-12-03 13:56:50 +0700

    Create init.sql

---
+++ init.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50),
+  email VARCHAR(50)
+);
+
+INSERT INTO users (name, email) VALUES
+  ('Alice', 'alice@example.com'),
+  ('Bob', 'bob@example.com'),
+  ('Tim', 'tim@example.com'),
+  ('Charlie', 'charlie@example.com');
